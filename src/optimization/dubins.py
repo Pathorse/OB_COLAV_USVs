783582eb8601ba9ea915896b04c49725caeace1a
Paal Arthur S. Thorseth <paalthorseth@gmail.com>
2020-05-04 10:25:59 +0200

    Update endpoint handling

--- src/optimization/dubins.py
+++ src/optimization/dubins.py
@@ -34,8 +34,33 @@ def dubins_path(wpts, theta_init=0, step_size=0.2, turning_radius=1, type='defau
 
         # Calculate desired ending heading
         if i + 1 == N - 1: # At last wpt
-            d_path.append(n_wpt)
-        else:
+
+            # Extract values
+            x   = n_wpt[0]
+            y   = n_wpt[1]
+            psi = theta1
+
+            # Dubins initial and final config
+            q0 = (c_wpt[0], c_wpt[1], theta1)
+            q1 = (n_wpt[0], n_wpt[1], theta1)
+
+            # Calculate dubins path between current and
+            # next waypoint
+            segment = dubins.shortest_path(q0, q1, turning_radius)
+            configurations, _ = segment.sample_many(step_size)
+
+            for config in configurations:
+
+                # Extract values
+                x   = config[0]
+                y   = config[1]
+                psi = config[2]
+
+                # Append to path
+                d_path.append([x, y, psi])
+
+        else: # Not at last wpt
+
             # Next next wpt
             nn_wpt   = wpts[i + 2]
 
@@ -49,21 +74,24 @@ def dubins_path(wpts, theta_init=0, step_size=0.2, turning_radius=1, type='defau
 
             # Calculate dubins path between current and
             # next waypoint
-            path = dubins.shortest_path(q0, q1, turning_radius)
-            configurations, _ = path.sample_many(step_size)
+            segment = dubins.shortest_path(q0, q1, turning_radius)
+            configurations, _ = segment.sample_many(step_size)
 
             for config in configurations:
 
                 # Extract values
-                x = config[0]
-                y = config[1]
+                x   = config[0]
+                y   = config[1]
+                psi = config[2]
 
                 # Append to path
-                d_path.append([x, y])
-
-            # Update heading
-            theta0 = theta1
+                d_path.append([x, y, psi])
 
 
     return d_path
 
+
+
+def dubins_sample_many(segments, step_size):
+
+    a = 0
