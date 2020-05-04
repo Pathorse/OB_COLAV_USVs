da1a045e5f6e5e37c33dcd346f7fcc0a734731b1
Paal Arthur S. Thorseth <paalthorseth@gmail.com>
2020-05-04 15:18:39 +0200

    Change to segmentation approach, add intial guess

--- src/optimization/dubins.py
+++ src/optimization/dubins.py
@@ -1,13 +1,17 @@
+import numpy as np
 from math import atan2, sqrt
 
 import dubins
 
 
 
-def dubins_path(wpts, theta_init=0, step_size=0.2, turning_radius=1, type='default'):
+def dubins_path(wpts, theta_init=0, turning_radius=1, type='default'):
 
     # Initialize dubins path
-    d_path = []
+    d_segments = []
+
+    # Initialize total path length
+    L = 0
 
     # Num wpts
     N = len(wpts)
@@ -47,17 +51,14 @@ def dubins_path(wpts, theta_init=0, step_size=0.2, turning_radius=1, type='defau
             # Calculate dubins path between current and
             # next waypoint
             segment = dubins.shortest_path(q0, q1, turning_radius)
-            configurations, _ = segment.sample_many(step_size)
 
-            for config in configurations:
+            # Find segment length
+            L_segment = segment.path_length()
 
-                # Extract values
-                x   = config[0]
-                y   = config[1]
-                psi = config[2]
+            # Update
+            d_segments.append(segment)
+            L += L_segment
 
-                # Append to path
-                d_path.append([x, y, psi])
 
         else: # Not at last wpt
 
@@ -75,23 +76,75 @@ def dubins_path(wpts, theta_init=0, step_size=0.2, turning_radius=1, type='defau
             # Calculate dubins path between current and
             # next waypoint
             segment = dubins.shortest_path(q0, q1, turning_radius)
-            configurations, _ = segment.sample_many(step_size)
 
-            for config in configurations:
+            # Find segment length
+            L_segment = segment.path_length()
 
-                # Extract values
-                x   = config[0]
-                y   = config[1]
-                psi = config[2]
+            # Update
+            d_segments.append(segment)
+            L += L_segment
 
-                # Append to path
-                d_path.append([x, y, psi])
+    return d_segments, L
 
+
+
+def dubins_sample_many(segments, step_size=0.2):
+
+    # Initialize path
+    d_path = []
+
+    for segment in segments:
+
+        # Extract configurations
+        configurations, _ = segment.sample_many(step_size)
+
+        for config in configurations:
+
+            # Extract values
+            x   = config[0]
+            y   = config[1]
+            psi = config[2]
+
+            # Append to path
+            d_path.append([x, y, psi])
 
     return d_path
 
 
+def dubins_sample(segments, t):
 
-def dubins_sample_many(segments, step_size):
+    # Initialize total dist
+    L = 0
+    for segment in segments:
 
-    a = 0
+        # Extract segment length
+        L_seg = segment.path_length()
+
+        # Check for correct segment
+        if t <= L + L_seg:
+
+            # Extract config at distance traveled
+            config = segment.sample(t - L)
+
+            # Extract values
+            x   = config[0]
+            y   = config[1]
+            psi = config[2]
+
+            return [x, y, psi]
+
+        # Add to total dist
+        L += L_seg
+
+    raise Exception('Failed to find dubins sample')
+
+
+def dubins_generate_initial_guess(segments, L, time_steps):
+
+    # Length stepsize
+    dL =  L/time_steps
+
+    # Sample state
+    x_guess = np.vstack([dubins_sample(segments, dL * t) for t in range(time_steps + 1)])
+
+    return x_guess
